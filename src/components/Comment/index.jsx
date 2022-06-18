035cd27ef25c8f09f18c587bbf389d387e7783ed
Allan Kruger <allan.kruger@compasso.com.br>
2022-06-18 15:53:53 +0100

    adds like feature

--- src/components/Comment/index.jsx
+++ src/components/Comment/index.jsx
@@ -1,10 +1,17 @@
 import { ThumbsUp, Trash } from "phosphor-react";
+import { useState } from "react";
 import { Avatar } from "../Avatar";
 import styles from "./styles.module.css";
 
 export function Comment({ content, onDeleteComment }) {
+  const [likeCount, setLikeCount] = useState(0);
+
   function handleDeleteComment() {
     onDeleteComment(content);
+  }
+
+  function handleLike() {
+    setLikeCount(likeCount + 1);
   }
 
   return (
@@ -29,9 +36,9 @@ export function Comment({ content, onDeleteComment }) {
         </div>
 
         <footer>
-          <button>
+          <button onClick={handleLike}>
             <ThumbsUp size={20} />
-            Like <span>20</span>
+            Like <span>{likeCount}</span>
           </button>
         </footer>
       </div>
