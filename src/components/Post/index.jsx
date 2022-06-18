0778fb759937e29d6399b9a4ca15bd6f8d6ae179
Allan Kruger <allan.kruger@compasso.com.br>
2022-06-18 14:56:24 +0100

    adds keys to maps on multiple pages

--- src/components/Post/index.jsx
+++ src/components/Post/index.jsx
@@ -45,10 +45,10 @@ export function Post({ author, publishedAt, content }) {
       <div className={styles.content}>
         {content.map((line) => {
           if (line.type === "paragraph") {
-            return <p>{line.content}</p>;
+            return <p key={line.content}>{line.content}</p>;
           } else if (line.type === "hyperlink") {
             return (
-              <p>
+              <p key={line.content}>
                 <a href="">{line.content}</a>
               </p>
             );
@@ -71,7 +71,7 @@ export function Post({ author, publishedAt, content }) {
 
       <div className={styles.commentList}>
         {comments.map((comment) => {
-          return <Comment content={comment} />;
+          return <Comment key={comment} content={comment} />;
         })}
       </div>
     </article>
